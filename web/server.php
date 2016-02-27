2e99ca703faa86d9fde59463c46a4185e4ca6ea0
Christopher Hammond <christopherhammond13@gmail.com>
2016-02-27 14:31:10 +0000

    test code

--- web/server.php
+++ web/server.php
@@ -1,5 +1,6 @@
 <?php
   require("inc/config.inc.php");
+  require ("inc/pusher/lib/Pusher.php");
 
   $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
@@ -7,8 +8,10 @@
     PUSHER_APP_KEY,
     PUSHER_APP_SECRET,
     PUSHER_APP_ID,
-    array('encrypted' => true);
+    array('encrypted' => true)
   );
 
+  $data['message'] = 'test';
+  $pusher->trigger('threewords', 'test_event', $data);
 
 ?>
