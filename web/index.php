5f3b9adf1c5f2e879477d5924620ac67d28629ec
Christopher Hammond <christopherhammond13@gmail.com>
2016-02-27 18:23:11 +0000

    beginning some pusher

--- web/index.php
+++ web/index.php
@@ -1,3 +1,6 @@
+<?php
+  require (dirname(__FILE__) . "/inc/config.inc.php");
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,7 +8,40 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/slate/bootstrap.min.css" rel="stylesheet" integrity="sha384-X9JiR5BtXUXiV6R3XuMyVGefFyy+18PHpBwaMfteb/vd2RrK6Gt4KPenkQyWLxCC" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet">
     <script src="https://js.pusher.com/3.0/pusher.min.js"></script>
+    <script src="http://code.jquery.com/jquery-2.2.1.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script src="https://cdn.rawgit.com/leggetter/pusher-js-client-auth/master/dist/pusher-js-client-auth.js"></script>
+
     <script>
+      var pusher = null;
+
+      function login()
+      {
+        var name = $('#nickname').val();
+        pusher = new Pusher(<?=PUSHER_APP_KEY; ?>,
+          {
+            authTransport: 'client',
+            clientAuth:
+            {
+              key: <?=PUSHER_APP_KEY; ?>,
+              secret: <?=PUSHER_APP_SECRET; ?>,
+              user_id: name,
+              user_info: {}
+            }
+          }
+        );
+      }
+
+
+      $(document).ready(function()
+      {
+        $('#loginModal').modal('show');
+        $('#play').click(function()
+        {
+          login();
+        });
+      });
+
       Pusher.log = function(message)
       {
         if (window.console && window.console.log)
@@ -14,13 +50,14 @@
         }
       };
 
-      var pusher = new Pusher('7d0b4730386735df8793',
-      {
-        encrypted: true
-      });
 
-      var channel = pusher.subscribe('threewords');
-      channel.bind('')
+      // var pusher = new Pusher('7d0b4730386735df8793',
+      // {
+      //   encrypted: true
+      // });
+
+      // var channel = pusher.subscribe('threewords');
+      // channel.bind('')
     </script>
   </head>
   <body>
@@ -35,11 +72,36 @@
             </li>
         </ul>
       </div>
-      <div class = "container">
+      <div class="container">
         <br>
-        <h1 align="center"> Choose The Words!</h1>
+        <h1 align="center">Choose The Words!</h1>
         <br>
       </div>
     </nav>
+    <div class="modal fade" id="loginModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">Choose a Nickname</h4>
+          </div>
+          <div class="modal-body">
+            <div class="container">
+              <h2>Please choose a nickname</h2>
+              <div class="col-md-12">
+                <form class="form-inline">
+                  <div class="form-group">
+                    <input type="text" id="nickname" maxlength="64" class="form-control" placeholder="Enter a Nickname" />
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" id="play" class="btn btn-primary">Let's play!</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
