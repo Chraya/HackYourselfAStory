71d726255d40c1d6d025f22470e1838581c84bf6
Christopher Hammond <christopherhammond13@gmail.com>
2016-02-28 01:08:17 +0000

    erhaps not, but this was....

--- web/webhook.php
+++ web/webhook.php
@@ -54,7 +54,7 @@
           $query = "INSERT INTO suggestions VALUES(NULL, " .
             $mysqli->real_escape_string($data['phrase']) . ", 1)";
 
-          erro_log(var_dump_err("About to run " . $query . "\n"));
+          error_log(var_dump_err("About to run " . $query . "\n"));
           $mysqli->query($query);
         }
       }
