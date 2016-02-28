a2bd7ab58af8ce228ad7a0bfde9bdf181b3052df
Christopher Hammond <christopherhammond13@gmail.com>
2016-02-28 01:12:52 +0000

    Fixed an awkward indexing issue

--- web/server.php
+++ web/server.php
@@ -49,7 +49,7 @@
       $suggestions = array("suggessions" => array());
       while ($row = $request->fetch_array(MYSQLI_ASSOC))
       {
-        $suggestions['suggestions'][$row['id']] = $row['threewords'];
+        $suggestions['suggestions'][(int)($row['id'])] = $row['threewords'];
       }
 
       SendToClients('vote_request', json_encode($suggestions));
